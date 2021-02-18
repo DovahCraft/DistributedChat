634251f4dffba09ffc51e6c2a5f0390a3f2e3dcd
Andrew Munoz <43388560+am009168@users.noreply.github.com>
2021-02-17 17:23:32 -0700

    Add files via upload

--- src/workers/Listener.java
+++ src/workers/Listener.java
@@ -1,36 +1,25 @@
 package workers;
 
-import com.company.ChatNode;
-import com.company.NodeInfo;
-
 import java.io.IOException;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Listener implements Runnable {
+public class Listener implements Runnable{
+    ServerSocket listenerSocket;
     Socket clientSocket;
-    Integer port;
-    public Listener(int port) {
-        this.port = port;
+    public Listener(ServerSocket inputSocket){
+        this.listenerSocket = inputSocket;
     }
-
     @Override
     public void run() {
-        System.out.println("Running listener, checking for new connections to Node");
-        try (ServerSocket listenerSocket = new ServerSocket(this.port)) {
-
-            synchronized (ChatNode.thisNode){
-                ChatNode.thisNode.setIp(InetAddress.getLocalHost().getHostAddress());
-            }
-            System.out.println("This node: " + ChatNode.thisNode.toString());
-            while(true) {
+        System.out.println("Running listener, checking for new connections.");
+        while(true){
+            try {
                 Thread listenerWorkThread = new Thread(new ListenerWorker(listenerSocket.accept()));
-                listenerWorkThread.start();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        }
-        catch(IOException e){
-            e.printStackTrace();
+
         }
     }
 }
--- src/workers/ListenerWorker.java
+++ src/workers/ListenerWorker.java
@@ -1,17 +1,19 @@
 package workers;
 
+import com.company.ChatNode;
+import com.company.NodeInfo;
+import message.*;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
-public class ListenerWorker implements Runnable {
+public class ListenerWorker implements Runnable{
     Socket chatSocket;
 
     public ListenerWorker(Socket inputSocket) {
         this.chatSocket = inputSocket;
     }
-
     @Override
     public void run() {
         System.out.println("Recieving message!");
@@ -19,13 +21,54 @@ public class ListenerWorker implements Runnable {
                 ObjectInputStream inputStream = new ObjectInputStream(chatSocket.getInputStream());
                 ObjectOutputStream outputStream = new ObjectOutputStream(chatSocket.getOutputStream())
         ) {
-            synchronized (System.out){
-                System.out.println("Chat node connected!");
+            System.out.println("Opened listenerWorker input and output streams successfully.");
+            //Mostly place holder information
+            NodeInfo node = new NodeInfo(chatSocket.getLocalSocketAddress().toString(), chatSocket.getPort(), "Name");
+            String fromClient = inputStream.readLine();
+            if(fromClient.equals("JOIN"))
+            {
+                flagType(Type.JOIN);
+            }
+
+            else if(fromClient.equals("QUIT"))
+            {
+                flagType(Type.LEAVE);
+            }
+
+            else
+            {
+                flagType(Type.CHAT);
             }
 
         } catch (IOException e) {
             System.out.println("Couldn't open client socket in ListenerWorker!");
             e.printStackTrace();
         }
+    }
+
+    public static void flagType(Type type)
+    {
+        switch(type)
+        {
+            case JOIN:
+                break;
+            case LEAVE:
+                break;
+            case CHAT:
+                break;
+            default:
+                System.out.print("INVALID MESSAGE TYPE");
+        }
+    }
+
+
+    public static void leaveChat(NodeInfo node )
+    {
+        //nodes.remove(node);
+    }
+
+    public static void joinChat(NodeInfo node)
+    {
+        //nodes.add(node);
     }
 }
