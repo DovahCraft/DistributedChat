6d4c7cd3007b8512394eeac0f4e5a1b5956c7a56
Jared Weinberger <jared.j.weinberger@gmail.com>
2021-02-26 13:22:06 -0700

    Can no longer test bc WSL messes up networking (#4)
    * Now sends messages
    
    * Flush output streams, removed lock from sender code
    
    * Made ParicipantsMap Serializable
    
    * Fun stuff
    
    * WSL Networking problem

--- src/com/company/ChatNode.java
+++ src/com/company/ChatNode.java
@@ -10,8 +10,6 @@ import workers.Sender;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Hashtable;
-import java.util.Map;
 
 import static com.company.Utils.*;
 
