a6fc5af219ba6369e9795207650cd45c06b5fb48
Andrew Munoz <43388560+am009168@users.noreply.github.com>
2021-02-27 21:28:24 -0700

    Update ParticipantsMap.java

--- src/com/company/ParticipantsMap.java
+++ src/com/company/ParticipantsMap.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.io.Serializable;
 import java.util.Hashtable;
-
+//Class to store our current Map of Participants
 public class ParticipantsMap extends Hashtable<NodeInfo, Boolean> implements Serializable {
 
 }
