058cb5657517729c3093258a7705a244cd075da1
Andrew Munoz <43388560+am009168@users.noreply.github.com>
2021-02-28 16:59:11 -0700

    Update JoinMessage.java

--- src/message/JoinMessage.java
+++ src/message/JoinMessage.java
@@ -2,11 +2,14 @@ package message;
 
 import com.company.NodeInfo;
 
+//Create message of type join
 public class JoinMessage extends Message {
+    //init variables
     final public String destinationIp;
     final public int destinationPort;
 
     public JoinMessage(MessageType messageType, NodeInfo source, String destinationIp, int destinationPort) {
+        //set variables
         super(messageType, source);
         this.destinationIp = destinationIp;
         this.destinationPort = destinationPort;
