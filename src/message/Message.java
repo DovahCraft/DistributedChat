8a6457e8d566cdca6042f84e5d64a54d5e83a115
Andrew Munoz <43388560+am009168@users.noreply.github.com>
2021-02-28 16:59:42 -0700

    Update Message.java

--- src/message/Message.java
+++ src/message/Message.java
@@ -3,10 +3,12 @@ package message;
 import com.company.NodeInfo;
 
 import java.io.Serializable;
-
+//Message class that is serializable
 public class Message implements Serializable {
+    //init variables
     public final MessageType messageType;
     public final NodeInfo source;
+    //Set variables
     public Message(MessageType messageType, NodeInfo source) {
         this.messageType = messageType;
         this.source = source;
