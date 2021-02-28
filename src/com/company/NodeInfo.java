b7c07b33ff880b941b694f8f339366dfb9de9f87
Andrew Munoz <43388560+am009168@users.noreply.github.com>
2021-02-27 21:28:02 -0700

    Update NodeInfo.java

--- src/com/company/NodeInfo.java
+++ src/com/company/NodeInfo.java
@@ -3,21 +3,25 @@ package com.company;
 
 import java.io.Serializable;
 
+//Create a class for our Node info that is Serializable
 public class NodeInfo implements Serializable {
+    //init variables
     public String ip;
     public int port;
     public String name;
-
+    
+    //assign variables 
     public NodeInfo(int inputPort, String inputName) {
         this.ip = null;
         this.port = inputPort;
         this.name = inputName;
     }
-
+    //set the current Node's IP
     public void setIp(String inputIp) {
         this.ip = inputIp;
     }
-
+    
+    //Check if two nodes are equal
     @Override
     public boolean equals(Object other) {
         if (other instanceof NodeInfo) {
@@ -28,7 +32,8 @@ public class NodeInfo implements Serializable {
         }
         return false;
     }
-
+    
+    //Print Node Information
     @Override
     public String toString() {
         return "IP: " + this.ip + " Port: " + this.port + " Name: " + this.name;
